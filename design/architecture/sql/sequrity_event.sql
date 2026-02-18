aef4ce2ba0f84450b4d4b1e8c7112c0528fcb151
Anton Lobanov <a.givertzman@icloud.com>
2026-02-18 13:50:52 +0300

    Design | Architecture | SecurityEvent

--- design/architecture/sql/sequrity_event.sql
+++ design/architecture/sql/sequrity_event.sql
@@ -3,15 +3,12 @@
 -- - Связь должна быть 1 : 1 (одному событию всегда соответствует один пользователь)
 
 CREATE TABLE security_event (
-    event_id: BIGINT NOT NULL
+    event_id BIGINT NOT NULL
         REFERENCES event(uid) ON DELETE CASCADE,
-    user_id: INT NOT NULL
+    user_id INT NOT NULL
         REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (event_id)
 );
-
-CREATE INDEX idx_security_event_event
-  ON security_event(event_id);
-
+-- Индекс для производительности запросов по пользователю и удалению
 CREATE INDEX idx_security_event_user
   ON security_event(user_id);
