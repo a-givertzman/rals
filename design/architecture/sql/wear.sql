d236a08825cf814f0ba02b86bcd7fd5e58d89b41
Anton Lobanov <a.givertzman@icloud.com>
2026-02-19 20:09:30 +0300

    Design | Architecture | Equipment | Износ

--- design/architecture/sql/wear.sql
+++ design/architecture/sql/wear.sql
@@ -1,18 +1,24 @@
 -- Степень состояния оборудования
 -- Используется для индикаторов и агрегированного состояния.
-
 CREATE TYPE severity_level AS ENUM (
     'normal',     -- состояние в пределах нормы
     'early',      -- ранние признаки отклонений
     'warning',    -- требуется внимание / планирование обслуживания
     'critical'    -- высокая вероятность отказа
 );
-
+-- Категория состояния оборудования
+-- Используется для индикаторов и агрегированного состояния.
+CREATE TYPE indicator_category AS ENUM (
+    'condition',    -- Индикаторы сосотояния (condition_indicator_value)
+    'operating'     -- Параметры режима (equipment_operating_snapshot)
+);
 -- Справочник диагностических индикаторов.
 -- Таблица статическая.
 -- Хранит описание типов индикаторов (не значения).
-CREATE TABLE condition_indicator (
+CREATE TABLE equipment_indicator (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    -- Категория condition, operating
+    category            indicator_category NOT NULL,
     -- Машинный код индикатора (например rms_vibration, bearing_temp)
     code                TEXT NOT NULL UNIQUE,
     -- Отображаемое имя для UI
@@ -25,14 +31,16 @@ CREATE TABLE condition_indicator (
     -- Индикатор может быть временно отключён
     is_active           BOOLEAN NOT NULL DEFAULT TRUE
 );
--- Индекс для выборки code
-CREATE INDEX idx_condition_indicator_code
-    ON condition_indicator (code);
 -- Индекс для выборки активных индикаторов
-CREATE INDEX idx_condition_indicator_active
-    ON condition_indicator (is_active);
+CREATE INDEX idx_equipment_indicator_active_true
+    ON equipment_indicator (id)
+    WHERE is_active = true;
 
--- История значений индикаторов (insert-only).
+-- Индекс для выборки по категории
+CREATE INDEX idx_equipment_indicator_category
+    ON equipment_indicator (category);
+
+-- История значений индикаторов (показателей) износа.
 -- Каждая запись — это состояние конкретного индикатора на момент времени.
 CREATE TABLE condition_indicator_value (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -41,17 +49,19 @@ CREATE TABLE condition_indicator_value (
         REFERENCES equipment(id) ON DELETE CASCADE,
     -- Тип индикатора
     indicator_id    BIGINT NOT NULL
-        REFERENCES condition_indicator(id) ON DELETE CASCADE,
+        REFERENCES equipment_indicator(id) ON DELETE CASCADE,
     -- Измеренное или рассчитанное значение
     value           DOUBLE PRECISION NOT NULL,
-    -- Классификация состояния на момент измерения
+    -- Классификация состояния на момент 
     severity        severity_level NOT NULL,
     -- Временная метка значения
-    created         TIMESTAMPTZ NOT NULL
+    created         TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 -- Для получения последних значений
 CREATE INDEX idx_civ_equipment_indicator_time
     ON condition_indicator_value (equipment_id, indicator_id, created DESC);
+CREATE INDEX idx_civ_equipment_created
+    ON condition_indicator_value (equipment_id, created DESC);
 -- Для операций очистки и retention
 CREATE INDEX idx_civ_created
     ON condition_indicator_value (created);
@@ -76,7 +86,7 @@ CREATE TABLE equipment_resource_state (
     -- Степень износа
     severity            severity_level NOT NULL,
     -- Время расчёта состояния
-    created             TIMESTAMPTZ NOT NULL
+    created             TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 -- Для выборки последнего состояния
 CREATE INDEX idx_ers_equipment_time
@@ -86,26 +96,28 @@ CREATE INDEX idx_ers_calculated_at
     ON equipment_resource_state (created);
 
 -- История усреднённых режимов эксплуатации.
--- Каждая запись описывает агрегированный режим за период.
+-- Каждая запись описывает агрегированный режим за произвольный период определяемый скоростью изменения.
 -- Для анализа износа и аномалий.
 CREATE TABLE equipment_operating_snapshot (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     -- Оборудование
-    equipment_id        BIGINT NOT NULL
+    equipment_id    BIGINT NOT NULL
         REFERENCES equipment(id) ON DELETE CASCADE,
-    -- Усреднённые параметры эксплуатации
-    avg_rpm             DOUBLE PRECISION,
-    max_load            DOUBLE PRECISION,
-    temperature_avg     DOUBLE PRECISION,
-    duty_cycle          DOUBLE PRECISION,
-    starts_count        INTEGER,
-    -- Время фиксации snapshot в БД
-    created             TIMESTAMPTZ NOT NULL DEFAULT now(),
-    CHECK (period_end > period_start)
+    -- Тип индикатора
+    indicator_id    BIGINT NOT NULL
+        REFERENCES equipment_indicator(id) ON DELETE CASCADE,
+    -- Измеренное или рассчитанное значение
+    value           DOUBLE PRECISION NOT NULL,
+    -- Степень выхода из нормального режима работы
+    severity        severity_level NOT NULL,
+    -- Временная метка значения
+    created         TIMESTAMPTZ NOT NULL DEFAULT now()
 );
--- Получение последних режимов
-CREATE INDEX idx_eos_equipment_period
-    ON equipment_operating_snapshot (equipment_id, period_start DESC);
--- Retention / архивация
-CREATE INDEX idx_eos_period_start
-    ON equipment_operating_snapshot (period_start);
+-- Для получения последних значений
+CREATE INDEX idx_eos_operating_snapshot_time
+    ON equipment_operating_snapshot (equipment_id, indicator_id, created DESC);
+CREATE INDEX idx_eos_operating_snapshot_created
+    ON equipment_operating_snapshot (equipment_id, created DESC);
+-- Для операций очистки и retention
+CREATE INDEX idx_eos_created
+    ON equipment_operating_snapshot (created);
